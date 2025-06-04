801a3bb6b15ed9f94066f74bdca07945d6ecbf27
Hong Yang <hooong.yang@gmail.com>
2025-06-04 12:04:14 -0400

    Remove duplicate import statement for torch.nn.functional in training.py to enhance code clarity.

--- activation_research/training.py
+++ activation_research/training.py
@@ -7,7 +7,6 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
 class SupConLoss(nn.Module):
